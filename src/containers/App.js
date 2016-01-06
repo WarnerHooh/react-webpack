c69616ee4c457c9834113c406967a6712003901c
WarnerHooh <Warner.Hooh@gmail.com>
2016-01-06 14:38:27 +0800

    Warner: fix issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from '../actions'
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from '../action'
 import AddTodo from '../components/AddTodo'
 import TodoList from '../components/TodoList'
 import Footer from '../components/Footer'
@@ -63,4 +63,6 @@ function select(state) {
 }
 
 // 包装 component ，注入 dispatch 和 state 到其
-默认的 connect(select)(App) 中；
+//默认的 connect(select)(App) 中；
+
+export default App;
